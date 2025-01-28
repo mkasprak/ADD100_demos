083801e7d2e03f732f5ca2d72e0fb3f11cd6ac28
Meri Kasprak <mkasprak@mchenry.edu>
2025-01-28 10:03:56 -0600

    new files

---
+++ fstring.py
@@ -0,0 +1,16 @@
+""" 
+    Introducing Formatting with fstring
+    Docstring example
+"""
+
+# Setting up variables
+var_1 = 2.3345
+var_2 = 2.24
+var_3 = "Meri"
+
+# Printing with fstring
+print(f"Hello, my name is {var_3}")
+print(f"We are going to format. var_1: {var_1:.2f}")
+print(f"{var_2:.1f}")
+
+# TODO add more fstring samples
---
+++ variables.py
@@ -0,0 +1,10 @@
+"""
+    Learning to use variables
+"""
+
+name = "Meri"
+age = 42
+personal_email = "meri @ myemail.com"
+print(name, age, personal_email)
+age = 50
+print(name, age, personal_email)
